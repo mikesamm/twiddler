1d42bb112ef995847d876e02ecff37f9063e9148
mike <sammartino.mike@gmail.com>
2024-02-29 11:24:36 -0600

    show tweet time in human friendly way

--- index.js
+++ index.js
@@ -3,21 +3,23 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
+  // add button inside a button div
+  const $buttonDiv = $('<div id=new-tweets-button>');
+  $body.prepend($buttonDiv);
+  $buttonDiv.append('<button id=new-tweets>New Tweets</button>');
+
   // create a div for all of the tweets
   const $tweetsDiv = $('<div id=tweets>');
   $body.append($tweetsDiv);
 
   // create time variables
   let previousTime; // would equal time listed in the timeStamp div
-  let now = Date.now();
 
 
   // deliver new tweets
   const newTweets = function() {
     // declare tweets variable
     let $tweets;
-    // clear the current tweets
-    $tweetsDiv.html('');
 
     // data-generator will generate 10 tweets off the bat, but then each next call to tweet generator is wrapped with a timeout
     // the time of the initial tweets should be set so it can be checked
@@ -30,21 +32,34 @@ $(document).ready(() => {
         // create a div for each tweet
         const $tweet = $('<div class=tweet></div>');
         // create div after each tweet div for timestamp
-        const $timeStamp = $('<div class=times>');
+        const $times = $('<div class=times>');
+        const $timeStamp = $('<div id=timestamp></div>');
+        const $timeago = $('<div id=timeago></div>');
         // text to go into tweet div
         const text = `@${tweet.user}: ${tweet.message}`;
         // text (time) to go into time div
         const time = moment();
-        const timeSince = moment().calendar(time); // ex: Today at 2:30 AM
-  
+        const timeTweeted = moment().calendar(time); // ex: Today at 2:30 AM
+        const timeago = moment().from();
+        
+        // add tweet text to tweet div
         $tweet.text(text);
-        $tweet.append($timeStamp);
-        $timeStamp.text(`${time} --${timeSince}--`);
+
+        // construct times and their divs
+        // add times container div
+        $tweet.append($times);
+        // add timestamp div to times div
+        $times.append($timeStamp);
+        // add timestamp
+        $timeStamp.text(`${timeTweeted}`);
+        // add timeago div to times div
+        $times.append($timeago);
+        // add timeago
+        $timeago.append(timeago);
   
         // set previousTime to the milliseconds of the tweet's created_at
         previousTime = tweet.created_at.getTime();
 
-  
         return $tweet;
       });
     } else {
@@ -55,35 +70,43 @@ $(document).ready(() => {
         // create a div for each tweet
         const $tweet = $('<div class=tweet></div>');
         // create div after each tweet div for timestamp
-        const $timeStamp = $('<div class=times>');
+        const $times = $('<div class=times>');
+        const $timeStamp = $('<div id=timestamp></div>');
+        const $timeago = $('<div id=timeago></div>');
         // text to go into tweet div
         const text = `@${tweet.user}: ${tweet.message}`;
         // text (time) to go into time div
         const time = moment();
-        const timeSince = moment().calendar(time); // ex: Today at 2:30 AM
-  
+        const timeTweeted = moment().calendar(time); // ex: Today at 2:30 AM
+        const timeago = moment().from();
+        
+        // add tweet text to tweet div
         $tweet.text(text);
-        $tweet.append($timeStamp);
-        $timeStamp.text(`${time} or ${timeSince}`);
+
+        // construct times and their divs
+        // add times container div
+        $tweet.append($times);
+        // add timestamp div to times div
+        $times.append($timeStamp);
+        // add timestamp
+        $timeStamp.text(`${timeTweeted}`);
+        // add timeago div to times div
+        $times.append($timeago);
+        // add timeago
+        $timeago.append(timeago);
   
         // set previousTime to the milliseconds of the tweet's created_at
         previousTime = tweet.created_at.getTime();
-  
+
         return $tweet;
       });
 
     }
 
     // append new tweets to tweets div
-    $tweetsDiv.append($tweets);
+    $tweetsDiv.prepend($tweets);
   };
   newTweets();
-
-
-  // add button inside a button div
-  const $buttonDiv = $('<div id=new-tweets-button>');
-  $body.append($buttonDiv);
-  $buttonDiv.append('<button id=new-tweets>New Tweets</button>');
 
   // add click handler to new tweet button
   const $tweetButton = $('#new-tweets');
@@ -95,5 +118,8 @@ $(document).ready(() => {
   $('#tweets').css('border', '2px solid red');
   $('#new-tweets-button').css('border', '2px solid red');
   $('.times').css('border', '2px solid blue');
+
+  // helper functions /////////////////////////////////////////////////////////
+
 
 });
